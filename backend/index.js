dc00c985e6f153a8ab685158f2a5b4bf8177a544
ravikhokle <ravikhokle1@gmail.com>
2024-12-27 23:38:03 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const DBConnect = require("./Models/db");
+const cors = require('cors');
+const AuthRouter = require('./Routes/AuthRouter');
+const PostRouter = require('./Routes/PostRouter')
+const ProfileRouter = require('./Routes/ProfileRouter');
+const bodyParser = require("body-parser");
+const path = require('path');
+
+const PORT = process.env.PORT || 8000;
+DBConnect();
+
+//path for frontend
+const forDirName = path.resolve();
+
+app.use(cors({ origin: '*', allowedHeaders: 'Authorization, Content-Type' }));
+app.use(bodyParser.json());
+app.use(cors({origin:"*"}));
+app.use('/profile', ProfileRouter);
+app.use('/auth', AuthRouter);
+app.use('/posts', PostRouter);
+app.use(express.json()) 
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(forDirName, "/frontend/dist")))
+app.get('*', (req,res)=>{
+    res.sendFile(path.resolve(forDirName, "frontend", "dist", "index.html"))
+});
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on PORT: ${PORT}`)
+})
