71f087a02c679a068f4cc10b558b1a5bd54ba387
ravikhokle <ravikhokle1@gmail.com>
2024-12-28 03:21:14 +0530

    last

--- backend/index.js
+++ backend/index.js
@@ -12,8 +12,6 @@ const path = require('path');
 const PORT = process.env.PORT || 8000;
 DBConnect();
 
-const _dirname = path.resolve();
-
 app.use(bodyParser.json());
 app.use(cors({origin:"*"}));
 app.use('/profile', ProfileRouter);
@@ -22,12 +20,6 @@ app.use('/posts', PostRouter);
 app.use(express.json()) 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use(cors({ origin: 'https://internship-kro.onrender.com', allowedHeaders: 'Authorization, Content-Type' }));
-
-app.use(express.static(path.join(_dirname, "/frontend/dist")))
-
-app.get('*', (req,res)=>{
-    res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
-});
 
 
 app.listen(PORT,()=>{
