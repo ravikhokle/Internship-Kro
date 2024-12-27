78aee431466aae3cc053f12f4ca378b3f9afa66a
Ravi Khokle <131489761+ravikhokle@users.noreply.github.com>
2024-12-28 04:25:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use('/auth', AuthRouter);
 app.use('/posts', PostRouter);
 app.use(express.json()) 
 app.use('/public', express.static(path.join(__dirname, 'public')));
-app.use(cors({ origin: 'https://internship-kro.onrender.com', allowedHeaders: 'Authorization, Content-Type' }));
+app.use(cors({ origin: 'https://internshipkro.netlify.app/', allowedHeaders: 'Authorization, Content-Type' }));
 
 
 app.listen(PORT,()=>{
