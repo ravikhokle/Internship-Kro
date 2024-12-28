f64043a53231e023e6093e14c2cd8ebb11eea963
Ravi Khokle <131489761+ravikhokle@users.noreply.github.com>
2024-12-28 18:53:23 +0530

    Update updateProfileImg.js

--- Controllers/updateProfileImg.js
+++ Controllers/updateProfileImg.js
@@ -4,7 +4,7 @@ const User = require('../Models/userModel');
 const updateProfile = async (req, res) => {
     try {
         const { _id } = req.query; 
-        const profileImgURL = `http://localhost:5000/public/images/profileImages/${req.file.filename}`;
+        const profileImgURL = `https://internship-kro.onrender.com/public/images/profileImages/${req.file.filename}`;
 
         if (!_id) {
             return res.status(400).json({ message: 'User ID is required.' });
